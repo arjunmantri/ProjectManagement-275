143722c6a88fdfae7cb2db8d49800bf922a99412
mahesh5539 <mahesh.dhamgunde@yahoo.com>
2015-11-20 18:41:48 -0800

    Modified request method

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,18 +1,26 @@
 package controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import dto.User;
 
 
 
@@ -24,12 +32,22 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @RestController
 @RequestMapping("/api/v1/*")
-public class UserController {
-
-	@RequestMapping(value="/user", method=RequestMethod.GET)
-	public ResponseEntity<String> getTest(){
-
-		return new ResponseEntity<String>("Mahesh, This is reply for your Test API !!!", HttpStatus.OK);
+public  class UserController {
+	
+	JSONObject response = new JSONObject();
+	
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<String> getTest(@RequestBody User reqPar){
+		String userName = reqPar.getUserName();
+		String password = reqPar.getPassword();
+		System.out.println("userName is "+userName+" & password is "+password);
+			try {
+				response.put("name", "mahesh");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+	    return new ResponseEntity<String>(response.toString(),HttpStatus.OK); 
 
 	}
 
