cfe283d6492053a1fee18b554910f21865c74273
Arjun Mantri <arjunmantri@users.noreply.github.com>
2015-12-02 13:51:28 -0800

    invite user

--- src/main/java/edu/sjsu/cmpe275/controller/UserController.java
+++ src/main/java/edu/sjsu/cmpe275/controller/UserController.java
@@ -75,8 +75,8 @@ public  class UserController {
 	void inviteUser(
 			@PathVariable String toEmailId,
 			@PathVariable long ProjectId) {
-		//String body = "http://localhost:3000/thome/"+toEmailId +"/";
-		String body = "http://localhost:8080/api/v1/project/dineshpandeysjsu@gmail";
+		String body = "http://localhost:3000/thome/dineshpandeysjsu@gmail.com/";
+		//String body = "http://localhost:8080/api/v1/project/dineshpandeysjsu@gmail";
 		System.out.println("----Invite user email----");
 		smtpMailSender.sendUserInviteEmailService(toEmailId, ProjectId, body);
 	}
